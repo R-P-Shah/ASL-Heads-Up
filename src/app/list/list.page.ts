5003675921eb15f1a4a10f40127894da7b6f4eb3
r-p-shah <rps9771@rit.edu>
2018-12-03 17:45:39 -0500

    add settings page

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -7,25 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
   private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
+  private gameModes = [
+    {
+      difficulty: 'Easy',
+      description: 'Dog Breeds'
+    },
+    {
+      difficulty: 'Hard',
+      description: 'Country Capitols'
+    }
   ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
+  public items: Array<{ title: string; note: string;}> = [];
+  
   constructor() {
-    for (let i = 1; i < 11; i++) {
+    for (let i = 0; i < this.gameModes.length; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        title: this.gameModes[i].description,
+        note: this.gameModes[i].difficulty
       });
     }
   }
