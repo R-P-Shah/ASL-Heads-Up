d511acb0fee9461a6f1ee9da644c5f626343fdc4
r-p-shah <rps9771@rit.edu>
2018-12-13 17:37:29 -0500

    refactor list page

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -8,12 +8,10 @@ import { GameInfoService } from '../game-info.service';
 })
 export class ListPage implements OnInit {
   //private selectedItem: any;
-  public timer = 60;
-  public tempDiff;
+  public gameModes = []; 
 
-  private gameModes = [];
-
-  public items: Array<{ description: string; difficulty: string;}> = [];
+  private selected = {};
+  public timer;
   
   //inject game-info service
   constructor(private games: GameInfoService) { }
@@ -23,14 +21,11 @@ export class ListPage implements OnInit {
 
 
   ngOnInit() {
+    //initialize
     this.gameModes = this.games.getGameModes();
-
-    for (let i = 0; i < this.gameModes.length; i++) {
-      this.items.push({
-        description: this.gameModes[i].description,
-        difficulty: this.gameModes[i].difficulty
-      });
-    }
+    this.timer = 60;
+    this.selected = this.gameModes[0];
+    
   }
   // add back when alpha.4 is out
   // navigate(item) {
